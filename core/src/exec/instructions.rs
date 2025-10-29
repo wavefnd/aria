87c13f251378bde35392b1e42b23a333b90f9103
LunaStev <youngjae681@gmail.com>
2025-10-29 14:26:05 +0900

    update

--- core/src/exec/instructions.rs
+++ core/src/exec/instructions.rs
@@ -1,1 +1,34 @@
-// instructions
+#[derive(Debug)]
+pub enum Instruction {
+    GetStatic(u16),
+    Ldc(u8),
+    InvokeVirtual(u16),
+    Return,
+    Unknown(u8),
+}
+
+impl Instruction {
+    pub fn from_bytecode(code: &[u8], pc: &mut usize) -> Self {
+        let opcode = code[*pc];
+        *pc += 1;
+        match opcode {
+            0xb2 => { //getstatic
+                let index = ((code[*pc] as u16) << 8) | (code[*pc + 1] as u16);
+                *pc += 2;
+                Instruction::GetStatic(index)
+            }
+            0x12 => { // ldc
+                let index = code[*pc];
+                *pc += 1;
+                Instruction::Ldc(index)
+            }
+            0xb6 => { // invokevirtual
+                let index = ((code[*pc] as u16) << 8) | (code[*pc + 1] as u16);
+                *pc += 2;
+                Instruction::InvokeVirtual(index)
+            }
+            0xb1 => Instruction::Return, // return
+            _ => Instruction::Unknown(opcode),
+        }
+    }
+}
--- core/src/runtime/frame.rs
+++ core/src/runtime/frame.rs
@@ -1,1 +1,24 @@
-// frame
+#[derive(Debug)]
+pub struct Frame {
+    pub local_vars: Vec<i32>,
+    pub operand_stack: Vec<i32>,
+    pub pc: usize,
+}
+
+impl Frame {
+    pub fn new(max_locals: usize, max_stack: usize) -> Self {
+        Self {
+            local_vars: vec![0; max_locals],
+            operand_stack: Vec::with_capacity(max_stack),
+            pc: 0,
+        }
+    }
+
+    pub fn push(&mut self, val: i32) {
+        self.operand_stack.push(val);
+    }
+
+    pub fn pop(&mut self) -> i32 {
+        self.operand_stack.pop().unwrap_or(0)
+    }
+}
